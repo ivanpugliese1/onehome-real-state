2fae15a3392b57713af77f5a24385ad3d95debaf
Ivan Pugliese <ivanpugliese.f@gmail.com>
2026-03-30 11:44:05 -0300

    Generalidades del proyecto finalizadas V2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ import { initTheme } from './utils/theme.js';
 import { initScroll } from './utils/scroll.js';
 import { initAnimations } from './utils/animations.js';
 import { initNav } from './utils/nav.js';
-import { propiedades } from './data/propiedades.js'; // eslint-disable-line no-unused-vars
 
 initTheme();
 initScroll();
